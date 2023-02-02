17fbb6b23c2317bc2d131d9bd788b6ac186d9bbf
Justin Huth <Jayhuth96@gmail.com>
2023-02-02 14:42:04 -0600

    finished both excersises

---
+++ OperatorExercise/Operators.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Operators
+    {
+        public static double AreaOfACircle()
+        {
+            Console.WriteLine("what is the raidus of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine($"the area of the circle is {area}, and {radius} is the radius.");
+            return area;
+        }
+    }
+}
--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+
+                Operators.AreaOfACircle();
+
+            Console.WriteLine($"Hello, World! {a} / {b} is {quotient} with {remainder} left over");
         }
     }
 }
